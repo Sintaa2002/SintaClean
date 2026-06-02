5ae60d3ebc69587e4f637f23f7d06e0f70b21868
Mateo <mateo.sintic@supra.hr>
2026-06-02 18:00:45 +0200

    opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,7 +62,7 @@ export const metadata: Metadata = {
     siteName: 'Šinta Clean',
     images: [
       {
-        url: 'https://sintaclean.hr/sintaclean-ciscenje-zagreb.webp',
+        url: 'https://sintaclean.hr/photos/sintaclean-ciscenje-zagreb.webp',
         width: 1200,
         height: 630,
       },

0b16eec868be3bbfc629004c6c248e746c6d8d9e
Mateo <mateo.sintic@supra.hr>
2026-06-29 13:26:05 +0200

    dodane slike radova

--- app/components/GalerijaRadovi.tsx
+++ app/components/GalerijaRadovi.tsx
@@ -5,9 +5,10 @@ export default function GalerijaRadovi() {
     <section className="py-8 px-6 md:px-20">
         <div className="max-w-6xl mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
             <BeforeAfterSlider
-                before="/photos/dubinsko-ciscenje-autosjedala-prije.webp"
-                after="/photos/dubinsko-ciscenje-autosjedala-poslije.webp"
+                before="/photos/dubinsko-ciscenje-tepiha-zapresic.webp"
+                after="/photos/dubinsko-ciscenje-tepiha-zagreb.webp"
             />
 
             <BeforeAfterSlider
@@ -16,8 +17,8 @@ export default function GalerijaRadovi() {
             />
 
             <BeforeAfterSlider
-                before="/photos/dubinsko-gepek-podloga-prije.webp"
-                after="/photos/dubinsko-gepek-podloga-poslije.webp"
+                before="/photos/dubinsko-tepiha.webp"
+                after="/photos/ciscenje-tepiha.webp"
             />
 
             <BeforeAfterSlider
@@ -26,13 +27,18 @@ export default function GalerijaRadovi() {
             />
 
             <BeforeAfterSlider
-                before="/photos/ciscenje-fuga-samobor.webp"
-                after="/photos/ciscenje-fuga-zagreb.webp"
+                before="/photos/dubinsko-gepek-podloga-prije.webp"
+                after="/photos/dubinsko-gepek-podloga-poslije.webp"
             />
 
             <BeforeAfterSlider
-                before="/photos/dubinsko-siceva-zagreb.webp"
-                after="/photos/dubinsko-siceva-zapresic.webp"
+                before="/photos/dubinsko-ciscenje-autosjedala-prije.webp"
+                after="/photos/dubinsko-ciscenje-autosjedala-poslije.webp"
+            />
+
+            <BeforeAfterSlider
+                before="/photos/ciscenje-fuga-samobor.webp"
+                after="/photos/ciscenje-fuga-zagreb.webp"
             />
             </div>
         </div>
